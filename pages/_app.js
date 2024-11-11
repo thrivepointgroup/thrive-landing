244ab78c2860501f81c9e41b0123e5f5587ef2f5
Carl Cortright <ckcortright@gmail.com>
2024-11-11 09:40:28 -0800

    add opengraph tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '@/styles/globals.css';
-import Head from 'next/head';  // Changed this line
+import Head from 'next/head';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 
@@ -8,7 +8,21 @@ export default function App({ Component, pageProps }) {
         <div className="min-h-screen flex flex-col">
             <Head>
                 <title>Thrive</title>
-                <meta name="description" content="Thrive Point Group" />
+                <meta name="description" content="Thrive is a mix of software engineering consultancy, holding company for high-growth software projects, and angel fund. It's owned and operated by Carl Cortright based in San Francisco CA." />
+
+                {/* Open Graph / Facebook */}
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://thrivepointgroup.com" />
+                <meta property="og:title" content="Thrive Point Group" />
+                <meta property="og:description" content="Thrive is a mix of software engineering consultancy, holding company for high-growth software projects, and angel fund. It's owned and operated by Carl Cortright based in San Francisco CA." />
+                <meta property="og:image" content="https://thrivepointgroup.com/og.png" />
+
+                {/* Twitter */}
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:url" content="https://thrivepointgroup.com" />
+                <meta name="twitter:title" content="Thrive Point Group" />
+                <meta name="twitter:description" content="Thrive is a mix of software engineering consultancy, holding company for high-growth software projects, and angel fund. It's owned and operated by Carl Cortright based in San Francisco CA." />
+                <meta name="twitter:image" content="https://thrivepointgroup.com/og.png" />
             </Head>
             <Navbar />
             <main className="flex-grow">
