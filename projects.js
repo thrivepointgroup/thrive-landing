7e4e0c8eb525b009bca3fb3f7e95b43b0dcd2e89
Carl Cortright <ckcortright@gmail.com>
2024-11-10 23:58:02 -0800

    update projects

--- projects.js
+++ projects.js
@@ -1,9 +1,15 @@
 export const projects = [
     {
         name: "Unnamed Meme Coin",
-        description: "Exploration in meme coins that would probably get me in trouble with HR.",
+        description: "Exploration that would probably get me in trouble with HR. (Coming Soon)",
         website: "",
         image: "/project/pump.jpg"
+    },
+    {
+        name: "Jane",
+        description: "Mental health and coaching benefits for high-performing companies. (Coming Soon)",
+        website: "",
+        image: "/project/jane.png"
     }
     // Add more projects as needed
 ];
