6f997858ad4be232e5b7d5b6efc0d205e07c29e1
Carl Cortright <ckcortright@gmail.com>
2024-11-11 00:38:49 -0800

    fix header

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import '@/styles/globals.css';
+import Head from 'next/head';  // Changed this line
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 
 export default function App({ Component, pageProps }) {
     return (
         <div className="min-h-screen flex flex-col">
+            <Head>
+                <title>Thrive</title>
+                <meta name="description" content="Thrive Point Group" />
+            </Head>
             <Navbar />
             <main className="flex-grow">
                 <Component {...pageProps} />
