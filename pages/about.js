6bcd1f9a60cab00a19e4874d60c151d2f5f613d3
Carl Cortright <ckcortright@gmail.com>
2025-02-04 10:04:16 -0800

    update copy

--- pages/about.js
+++ pages/about.js
@@ -33,28 +33,28 @@ export default function About() {
       </div>
       <div className="space-y-6 text-lg">
         <p>
-          Hi! My name is Carl and I'm a software engineer and entrepreneur based in San Francisco, CA.
-          I have 10+ years of software engineering and entrepreneurship experience across venture-backed startups,
-          scale ups, and public companies. I started Thrive to work on ideas that I'm personally excited about,
-          while helping companies in my orbit overcome their tech challenges. We both build and invest to
-          create the best outcomes. Thrive is part consultancy, part holding company for my projects, and part angel fund.
+          Hi! My name is Carl, and I'm a software engineer and entrepreneur based in San Francisco, CA.
+          I have over 10 years of experience in software engineering, entrepreneurship, and investing across venture-backed startups,
+          scale-ups, public companies, and venture funds. I founded Thrive to pursue ideas I’m personally excited about while helping companies in my orbit.
+          We are active builders and investors. Thrive is part advisory and consultancy, part holding company, and part fund.
         </p>
         <p>
-          I started my career at Coinbase where I fast-tracked my way to Senior Software Engineer. At Coinbase I wore
-          many hats across commerce, decentralized exchange, regular exchange, and core financial infrastructure.
-          I've written code that has moved trillions of dollars (yes, with a T),
-          secured hundreds of billions of dollars, and served hundreds of millions of users. While at
-          Coinbase I was also the only technical member of the Coinbase ventures investment committee,
-          where I owned the dev tool and protocol investment areas and made ~40 early-stage
-          cryptocurrency investments.
+          I started my career at Coinbase, where I fast-tracked advanced to Senior Software Engineer. During my time there, I wore many hats,
+          working in commerce, decentralized exchange, centralized exchange, and core financial infrastructure. I’ve written code that has moved trillions of dollars (yes, with a T),
+          secured hundreds of billions of dollars, and served hundreds of millions of users. In addition, I was the sole technical member of the Coinbase Ventures investment committee,
+          where I managed the dev tools and protocol investment areas, making approximately 40 early-stage cryptocurrency investments and contributing to hundreds more.
         </p>
         <p>
-        After Coinbase I started my own company in the Web3 space as a technical CEO. I raised $5.3m from investors like Kindred Ventures, Matchstick, Foundry Group, Coinbase Ventures, and Chapter One. I led of team of 10, owning engineering and go-to-market. I've taken many products, ranging from simple SaaS applications to heavily technical data infrastructure, to market. As a CTO or technical member of the team, I understand both the business and what needs to happen technically to achieve the company's goals. My latest venture before I started Thrive was a YC-backed AI company where I was CTO. I actively angel invest through the Thrive entity and have ownership in ~30 and counting early stage tech startups.
+          After Coinbase, I started my own company as a technical CEO. I raised $5.3M from investors including Kindred Ventures, Matchstick, Foundry Group, Coinbase Ventures, and Chapter One.
+          I led a team of 10, overseeing both engineering and go-to-market strategies. I have taken many products to market—ranging from simple SaaS applications to complex data infrastructure solutions.
+          I consider myself a deeply technical generalist who understands both the business and the technical steps required to achieve company goals.
+          My most recent venture before founding Thrive was a YC-backed AI company, where I served as CTO.
+          I also actively invest through Thrive and currently hold stakes in approximately 30 early-stage tech startups.
         </p>
         <p>
-          Thrive is combining everything that I've ever learned about building companies and investing into a single entity. 
-          It's a way for me to build software, work with other entrepreneurs, and effectively deploy capital into ecosystems
-           that I believe in. I write actively on my blog, which you can check out above.
+          Thrive combines everything I've learned about building companies and investing into a single entity.
+          It allows me to build software, collaborate with other entrepreneurs, and effectively deploy capital into ecosystems I believe in.
+          I also write actively on my blog, which you can check out above.
         </p>
         <p>
           You can read more about my professional history on my{' '}
