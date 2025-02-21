112c1da251bc7ff5b7beeaa586542a20fb16cfed
Carl Cortright <ckcortright@gmail.com>
2025-02-21 09:35:14 -0800

    🧩

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 // pages/index.js
 import Balance from '../components/Balance';
+import Link from 'next/link';
 
 export default function Home() {
     return (
         <div className="min-h-screen flex flex-col items-center justify-center px-4 sm:px-6 lg:px-8">
-            <div className="mb-8">
+            <Link href="/balance" className="mb-8">
                 <Balance width={250} height={250} fadeToLogo={true} />
-            </div>
+            </Link>
             <p className="max-w-[600px] text-lg text-justify font-normal">
                 Thrive is a mix of consultancy and advisory, holding company for high-growth software projects, and fund. It's owned and
                 operated by Carl Cortright based in San Francisco CA.
